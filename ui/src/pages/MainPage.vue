8528363e4959ec212abc7c2cecb529ea03bcc4f6
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2025-05-17 14:14:00 +0200

    Done.

--- ui/src/pages/MainPage.vue
+++ ui/src/pages/MainPage.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-// @ts-nocheck - Disable TypeScript checking for this file
-import { PlBlockPage, PlDropdownRef, PlAgDataTable, PlSlideModal, PlBtnGhost, PlMaskIcon24, PlDropdownMulti } from '@platforma-sdk/ui-vue';
-import type { PlRef, PlDataTableSettings } from '@platforma-sdk/model';
+import { liabilityTypes } from '@platforma-open/milaboratories.antibody-sequence-liabilities.model';
+import type { PlRef } from '@platforma-sdk/model';
 import { plRefsEqual } from '@platforma-sdk/model';
+import type { PlAgDataTableSettings } from '@platforma-sdk/ui-vue';
+import { PlAgDataTableV2, PlBlockPage, PlBtnGhost, PlDropdownMulti, PlDropdownRef, PlMaskIcon24, PlSlideModal } from '@platforma-sdk/ui-vue';
 import { computed, ref } from 'vue';
 import { useApp } from '../app';
 
@@ -11,47 +12,26 @@ const app = useApp();
 function setInput(inputRef?: PlRef) {
   if (!inputRef) return;
   app.model.args.inputAnchor = inputRef;
-  app.model.args.clonotypingRunId = inputRef?.blockId;
 
-  const title = app.model.outputs.inputOptions?.find((o) => plRefsEqual(o.ref, inputRef))?.label;
-
-  if ((inputRef.name.split('/')[1] == 'abundance') || (inputRef.name.split('/')[1] == 'read-count')) {
-    app.model.args.isSingleCell = true;
-    app.model.args.chain = undefined;
-  } else {
-    app.model.args.isSingleCell = false;
-    app.model.args.chain = inputRef.name.split('/')[1];
-  }
+  const label = app.model.outputs.inputOptions?.find((o) => plRefsEqual(o.ref, inputRef))?.label ?? '';
 
   // Set title to dataset label
-  app.model.args.title = title;
+  app.model.ui.title = 'Antibody Sequence Liabilities - ' + label;
 }
 
-const tableSettings = computed<PlDataTableSettings>(() => ({
-  sourceType: 'ptable',
-  pTable: app.model.outputs.pt?.table,
- // sheets: app.model.outputs.pt?.sheets,
-}));
+const tableSettings = computed<PlAgDataTableSettings>(() => {
+  const pTable = app.model.outputs.pt;
+  if (pTable === undefined && !app.model.outputs.isRunning) {
+    // special case: when block is not yet started at all (no table calculated)
+    return undefined;
+  }
+  return {
+    sourceType: 'ptable',
+    model: pTable,
+  };
+});
 
 const settingsIsShown = ref(app.model.args.inputAnchor === undefined);
-
-const liabilityTypesOptions = computed(() => {
-  const liabilities = [
-    { value: 'Deamidation (N[GS])', label: 'Deamidation (N[GS])' },
-    { value: 'Fragmentation (DP)', label: 'Fragmentation (DP)' },
-    { value: 'Isomerization (D[DGHST])', label: 'Isomerization (D[DGHST])' },
-    { value: 'N-linked Glycosylation (N[^P][ST])', label: 'N-linked Glycosylation (N[^P][ST])' },
-    { value: 'Deamidation (N[AHNT])', label: 'Deamidation (N[AHNT])' },
-    { value: 'Hydrolysis (NP)', label: 'Hydrolysis (NP)' },
-    { value: 'Fragmentation (TS)', label: 'Fragmentation (TS)' },
-    { value: 'Tryptophan Oxidation (W)', label: 'Tryptophan Oxidation (W)' },
-    { value: 'Methionine Oxidation (M)', label: 'Methionine Oxidation (M)' },
-    { value: 'Deamidation ([STK]N)', label: 'Deamidation ([STK]N)' },
-    { value: 'Missing Cysteines', label: 'Missing Cysteines' },
-    { value: 'Extra Cysteines', label: 'Extra Cysteines' },
-  ];
-  return liabilities;
-});
 
 const liabilityTypesModel = computed({
   get: () => (app.model.args.liabilityTypes ?? []),
@@ -73,7 +53,7 @@ const liabilityTypesModel = computed({
         </template>
       </PlBtnGhost>
     </template>
-    <PlAgDataTable v-model="app.model.ui.tableState" :settings="tableSettings" show-export-button />
+    <PlAgDataTableV2 v-model="app.model.ui.tableState" :settings="tableSettings" show-export-button />
   </PlBlockPage>
 
   <PlSlideModal v-model="settingsIsShown">
@@ -86,7 +66,7 @@ const liabilityTypesModel = computed({
       required
       @update:model-value="setInput"
     />
-    <PlDropdownMulti v-model="liabilityTypesModel" label="Liability types" :options="liabilityTypesOptions" >
+    <PlDropdownMulti v-model="liabilityTypesModel" label="Liability types" :options="liabilityTypes" >
       <template #tooltip>
         Select the liability types to include in the analysis.
       </template>
